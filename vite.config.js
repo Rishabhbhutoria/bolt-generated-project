a346ab3e3e6abbc978a94b4cdf893aacae1c3d25
Rishabhbhutoria <78445765+Rishabhbhutoria@users.noreply.github.com>
2025-02-02 18:37:14 +0530

    Initial commit from your app

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,6 @@
 import { defineConfig } from 'vite';
-    import react from '@vitejs/plugin-react';
+  import react from '@vitejs/plugin-react';
 
-    export default defineConfig({
-      plugins: [react()],
-      build: {
-        rollupOptions: {
-          external: ['pdfjs-dist/build/pdf.worker.entry']
-        }
-      }
-    });
+  export default defineConfig({
+    plugins: [react()]
+  });
